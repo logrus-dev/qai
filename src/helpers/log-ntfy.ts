1cb4bf25824bb65dadccb60eefbd00dbfa9d478b
Igor Kesler <i.kesler@logrus.dev>
2024-12-18 13:51:17 +0300

    remove sonic boom - no need, low load

--- src/helpers/log-ntfy.ts
+++ src/helpers/log-ntfy.ts
@@ -1,13 +1,8 @@
 import build from 'pino-abstract-transport';
-import { SonicBoom } from 'sonic-boom';
-import { once } from 'events';
 
 const { NTFY_ENDPOINT, NTFY_TOPIC, NTFY_USER, NTFY_PASSWORD } = process.env;
 
 export default async function (opts: any) {
-  const destination = new SonicBoom({ dest: opts.destination || 1, sync: false });
-  await once(destination, 'ready');
-
   return build(async function (source) {
     for await (let obj of source) {
       if (NTFY_ENDPOINT && NTFY_TOPIC && NTFY_USER && NTFY_PASSWORD) {
@@ -24,13 +19,6 @@ export default async function (opts: any) {
           }),
         });
       }
-
-      await once(destination, 'drain');
     }
-  }, {
-    async close (err) {
-      destination.end()
-      await once(destination, 'close');
-    }
-  })
+  });
 };
